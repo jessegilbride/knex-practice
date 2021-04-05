efc8265a1675aaceb19f1b1b699729d68c171be1
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-05 16:27:56 -0400

    initial upload of blogful SQL setup files

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT into blogful_articles (title, content, date_published)
+VALUES
+  ('blog post 1',   '1 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '1 days'::INTERVAL),
+  ('blog post 2',   '2 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '2 days'::INTERVAL),
+  ('blog post 3',   '3 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '3 days'::INTERVAL),
+  ('blog post 4',   '4 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '4 days'::INTERVAL),
+  ('blog post 5',   '5 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '5 days'::INTERVAL),
+  ('blog post 6',   '6 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '6 days'::INTERVAL),
+  ('blog post 7',   '7 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '7 days'::INTERVAL),
+  ('blog post 8',   '8 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '8 days'::INTERVAL),
+  ('blog post 9',   '9 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '9 days'::INTERVAL),
+  ('blog post 10', '10 ... Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.', now() - '10 days'::INTERVAL),
+  ('blog post 11', '11 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '11 days'::INTERVAL),
+  ('blog post 12', '12 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '12 days'::INTERVAL),
+  ('blog post 13', '13 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '13 days'::INTERVAL),
+  ('blog post 14', '14 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '14 days'::INTERVAL),
+  ('blog post 15', '15 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '15 days'::INTERVAL),
+  ('blog post 16', '16 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '16 days'::INTERVAL),
+  ('blog post 17', '17 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '17 days'::INTERVAL),
+  ('blog post 18', '18 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '18 days'::INTERVAL),
+  ('blog post 19', '19 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '19 days'::INTERVAL),
+  ('blog post 20', '20 ... Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.', now() - '20 days'::INTERVAL)
+;
