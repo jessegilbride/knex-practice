3c5a4ac32363f24a7207f40e823af40485362773
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-06 19:05:24 -0400

    initial upload

---
+++ src/articles-service.js
@@ -0,0 +1,8 @@
+const ArticlesService = {
+  getAllArticles(knex) {
+    // return Promise.resolve('all the articles!')
+    return knex.select('*').from('blogful_articles')
+  }
+};
+
+module.exports = ArticlesService;
