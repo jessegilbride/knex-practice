d9534f5e87e21483cab0d184473cd31b07aaf3d3
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-16 15:10:48 -0400

    finish deleteItem functionality

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -34,57 +34,34 @@ const ShoppingListService = {
     return knex('shopping_list').where({ id }).update(newItemData);
   },
   addItem(knex, newItem) {
-    // knex.select("*").from('shopping_list').then(response => console.log(response));
     return knex
       .returning('*')
-      // .returning(['id','name','price','category','checked','date_added'])
       .insert(newItem)
       .into('shopping_list')
-      // .returning('*')
-      // .returning(['id','name','price','category','checked','date_added'])
-      // .then((result) => {
-      //   console.log(result);
-      //   return result
-      // })
       .then((rows) => {
-        // console.log(rows[0]) // <<<<<<<<<<<<< WHAT'S THIS TELLING ME? 
         return rows[0]
       })
       .then((result) => {
         // console.log(result);
-        // return { ...items, price: Number(items.price) }; // original -- works only for an empty list
         return { ...result, price: Number(result.price) };
-        // console.log(typeof(result));
-        // console.log(result);
-        // return result.map((row) => {
-        //   // console.log(row);
-        //   return { ...row, price: Number(row.price) };
-        // });
-      })
-      .catch(function(err) {
-        console.log('OH NOOOOOOOOOOOOES!...')
-        console.error(err);
-      })
+      });
   },
   deleteItem(knex, id) {
-    // find item id, store its array index as "idIndex"
-    // const testItems;
     return knex
       .from('shopping_list')
       .select('*')
       .then(listItems => {
         const itemToDeleteIndex = listItems.findIndex(itemToDelete => itemToDelete.id === id);
-        return listItems.splice(itemToDeleteIndex, 1);
-      })
-      // .then(itemToDelete => {
-      //   return knex
-      //     .from('shopping_list')
-      //     .select('*')
-      //     .then()
-      // })
-      // .then(results => console.log(results)); // for now, this chain only finds the index of the thing to delete :P
+        listItems.splice(itemToDeleteIndex, 1);
+        // const deletedItem = listItems.splice(itemToDeleteIndex, 1); // for use in case of need to compare what's been deleted
+        // console.log(deletedItem);
+        // return {listItems, deletedItem};
 
-    // testItems.splice(idIndex, 1);
+
+        return listItems.map(row => {
+          return { ...row, price: Number(row.price) }
+        })
+      })
   }
 };
 
